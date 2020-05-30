1de97c5d2188ed7dbc3778ee4d87ca202a5961ce
andreygora <andreygora18@gmail.com>
2020-05-30 15:14:03 +0300

    Update main.cpp
     cout << "Hello from adreygora" <<

--- 12/main.cpp
+++ 12/main.cpp
@@ -4,13 +4,7 @@ using namespace std;
 
 int main()
 {
-   // cout << "Hello world!" << endl;
-   // cout << "Hello world! I am Darth_Miguel!"<<endl;
-   // cout << "Hello world! I am Darth_Miguel or not hahah!"<<endl;
-   // cout << "Qqq world!"<<endl;
-   // cout << "Hello everyone" << endl;
-    cout << "Goodbye!" << endl;
+ cout << "Hello from adreygora" << endl;
     return 0;
-    //Hai Team
-
+}
 }
